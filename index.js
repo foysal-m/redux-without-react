36611d9492a7a1738a8d5ba5ae24d9c7fc92025f
Foysal Mohammad <foysal@earlblue.com>
2021-09-14 22:24:38 +0100

    combined reducers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const redux = require('redux')
+const combineReducers = redux.combineReducers
 
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
   //but action creator is  a funtion that returns an action object
@@ -12,11 +14,22 @@ function buyCake() {
     // action object has type property (normally a string constant) const BUY_CAKE = 'BUY_CAKE'
   }
 }
+function buyIceCream() {
+  //but action creator is  a funtion that returns an action object
+  return {
+    type: BUY_ICECREAM,
+    info: 'first redux action', //action is just an object
+    // action object has type property (normally a string constant) const BUY_CAKE = 'BUY_CAKE'
+  }
+}
 
-const initialState = {
+const initialNumOfCake = {
   numOfCakes: 10,
 }
-const reducer = (state = initialState, action) => {
+const initialNumOfIceCream = {
+  numofIceCreams: 20,
+}
+const cakeReducer = (state = initialNumOfCake, action) => {
   //reducers specify how the app's state changes in response to actions sent the store.
   //only describes what happened
   // but not how app state changed.
@@ -30,13 +43,33 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+const IceCreamReducer = (state = initialNumOfIceCream, action) => {
+  //reducers specify how the app's state changes in response to actions sent the store.
+  //only describes what happened
+  // but not how app state changed.
+  //reducer is a function that accept two arguments (state, action) and returns a new state of the application
+  //reducer is a pure function
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return { ...state, numofIceCreams: state.numofIceCreams + 20 }
 
-const store = createStore(reducer) //one store for the entire application
+    default:
+      return state
+  }
+}
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: IceCreamReducer,
+})
+const store = createStore(rootReducer) //one store for the entire application
 
 console.log(store.getState().numOfCakes) //allows access to state vi getState() method
 const unsubscribe = store.subscribe(() => console.log(store.getState())) // register listener via subscribe() method
 store.dispatch(buyCake()) //allows state to be updated via dispatch(action)
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe() // unsubscribe listener via result of calling subscribe method
 store.dispatch(buyCake())
